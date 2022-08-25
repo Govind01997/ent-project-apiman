e59f24a042e2e89a789ff8af1fd7a915f1ac6e6d
govind <govindmundhe01997@gmail.com>
2022-08-25 15:52:20 +0530

    call External,internal and strapi service from k8s

--- src/main/java/com/entando/apiman/k8sService/InternalServiceFetcher.java
+++ src/main/java/com/entando/apiman/k8sService/InternalServiceFetcher.java
@@ -2,78 +2,85 @@ package com.entando.apiman.k8sService;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import io.kubernetes.client.openapi.ApiException;
 import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.models.V1Pod;
 import io.kubernetes.client.openapi.models.V1PodList;
 import io.kubernetes.client.openapi.models.V1ServiceList;
 
 @Component
 public class InternalServiceFetcher {
 
+	@Autowired
+	K8ServiceConnector kConnector;
 
-   
-    @Autowired
-    K8ServiceConnector kConnector;
+	List<String> ClusterIP;
 
+	String ContextPath;
 
-    String ClusterIP;
+	Integer Port;
 
-    String ContextPath;
-    
-    Integer Port;
+	public String fetchService() throws FileNotFoundException, IOException, ApiException {
 
-    public String fetchService() throws FileNotFoundException, IOException, ApiException {
+		CoreV1Api api = kConnector.getV1API();
 
-        CoreV1Api api = kConnector.getV1API();
+		String ns = "entando";
+		String ls = "entando.org/deployment=pn-baf96d56-1f31cbd9-akhileshprajapatinv-ent-project-template";
+		String fs = "status.phase=Running";
+		V1ServiceList list = api.listNamespacedService(ns, "", null, null, null, null, null, null, null, null, null);
 
-        V1ServiceList list = api.listNamespacedService("entando", "", null, null, null, "EntandoPlugin", null, null, null, null, null);
+		list.getItems().forEach(e -> {
+			try {
+				V1PodList ll = api.listNamespacedPod(ns, "pretty", null, null, null, null, null, null, null, null,
+						null);
 
+				ClusterIP = e.getSpec().getClusterIPs();
+				// System.out.println(ClusterIP);
 
-        list.getItems().forEach(e->{
-        try {
+				e.getSpec().getPorts().stream().forEach(p -> {
+					Port = p.getPort();
+				});
 
-            
+				System.out.println(ClusterIP + ":" + Port);
 
-            V1PodList ll = api.listNamespacedPod("entando", "pretty", null, null, null,"EntandoPlugin="+ e.getMetadata().getLabels().get("entando.org/deployment").toString(), null, null, null, null, null);
-         
+//				for (V1Pod item : ll.getItems()) {
+//					ContextPath = item.getMetadata().getName();
+//				}
 
+				try {
 
-            ClusterIP = e.getSpec().getClusterIP();
+					ll.getItems().forEach(l -> {
+						l.getSpec().getContainers().forEach(cd -> {
+							// Port = cd.getPorts().get(0).getHostPort().toString();
+							cd.getEnv().forEach(ge -> {
+//                            if(ge.getName().equals("SERVER_SERVLET_CONTEXT_PATH"))
+//                            {
+								ContextPath = ge.getValue();
 
-           e.getSpec().getPorts().stream().forEach(p -> {Port =p.getPort();});
+								System.out.println(ClusterIP + ":" + Port + "/" + ContextPath);
+//                            }
 
+							});
+						});
+						;
+					});
+				} catch (Exception e2) {
+					System.out.println("no value");
+				}
 
-            ll.getItems().forEach(l->{l.getSpec().getContainers().forEach(
-                    cd->
-                    {
-                        // Port = cd.getPorts().get(0).getHostPort().toString();
-                        cd.getEnv().
-                        forEach(ge->{ 
-                            if(ge.getName().equals("SERVER_SERVLET_CONTEXT_PATH"))
-                            {
-                               ContextPath = ge.getValue();
+			} catch (ApiException e1) {
 
-                               
-                            }
-        
-                        }); }
-                    );;} );
+				e1.printStackTrace();
+			}
+			;
+		});
 
-                   
-        } catch (ApiException e1) {
-           
-            e1.printStackTrace();
-        }
-    ;} );
+		return ClusterIP + ":" + Port + ContextPath;
 
-
-   return ClusterIP+":"+Port+ContextPath;
-
-
-        
-    }
+	}
 }
--- src/main/java/com/entando/apiman/rest/EntApimanController.java
+++ src/main/java/com/entando/apiman/rest/EntApimanController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.entando.apiman.k8sService.ExternalServiceFetcher;
 import com.entando.apiman.k8sService.InternalServiceFetcher;
 
 import io.kubernetes.client.openapi.ApiException;
@@ -18,18 +19,23 @@ import io.kubernetes.client.openapi.ApiException;
 @RequestMapping("/fetcher")
 public class EntApimanController {
 
-
 	@Autowired
 	InternalServiceFetcher iFetcher;
-
-
-
+	//ExternalServiceFetcher eFetcher;
+  
 	@GetMapping("/")
 	public String InternalData() throws FileNotFoundException, IOException, ApiException {
 
-
 		return iFetcher.fetchService();
-		
+
 	}
 
+	@Autowired
+	  ExternalServiceFetcher efetcher;
+	
+	@GetMapping("/external")
+	public String externalData() throws FileNotFoundException, IOException, ApiException {
+        System.out.println("before xfetch");
+		return efetcher.xfetchService();
+	}
 }
